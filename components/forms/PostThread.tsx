0bc1ff9f21573601e8d41b9c205da3ef3c5020a7
NhanLe418 <nhan.lethanh@toppa.vn>
2023-08-27 21:51:11 +0700

    move clerk folder out of uploadthing

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -18,6 +18,7 @@ import { Textarea } from "../ui/textarea";
 import { usePathname, useRouter } from "next/navigation";
 import { ThreadValidation } from "@/lib/validation/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 
 interface Props {
 	user: {
@@ -34,6 +35,7 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
 	const router = useRouter();
 	const pathname = usePathname();
+	const { organization } = useOrganization();
 
 	const form = useForm({
 		resolver: zodResolver(ThreadValidation),
@@ -47,7 +49,7 @@ function PostThread({ userId }: { userId: string }) {
 		await createThread({
 			text: values.thread,
 			author: userId,
-			communityId: null,
+			communityId: organization ? organization.id : null,
 			path: pathname,
 		});
 
